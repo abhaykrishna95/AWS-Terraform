1228631eb190d24e600d7dff44d08a9344828ce6
abhaykrishna95 <abhaykrishna.k26@gmail.com>
2022-11-29 23:57:20 +0530

    tf fmt

--- aws-infra-guacd/locals.tf
+++ aws-infra-guacd/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     environment = var.environment
     project     = var.project
-    deployment = "managed by terraform"
+    deployment  = "managed by terraform"
   }
   name_prefix = "${var.project}-${var.environment}"
 
--- aws-infra-guacd/vpc-network.tf
+++ aws-infra-guacd/vpc-network.tf
@@ -22,7 +22,7 @@ data "aws_availability_zones" "available" {}
 
 #Create the VPC
 resource "aws_vpc" "vpc" {
-  
+
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
   tags = {
