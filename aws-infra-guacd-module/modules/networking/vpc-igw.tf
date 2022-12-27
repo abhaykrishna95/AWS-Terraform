882a4352b4cde08314c0a31b202e1553c0bcb486
abhaykrishna95 <89457775+abhaykrishna95@users.noreply.github.com>
2022-12-27 19:40:48 +0530

    master commit

---
+++ aws-infra-guacd-module/modules/networking/vpc-igw.tf
@@ -0,0 +1,43 @@
+### CUSTOM VPC CONFIGURATION
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_vpc" "vpc" {
+
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    terraform = "true"
+    Name      = "${var.name_prefix}-vpc"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+### INTERNET GATEWAY
+
+#Create Internet Gateway and attach it to VPC
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    terraform = "true"
+    Name      = "${var.name_prefix}-igw"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
